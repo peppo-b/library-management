1f28e233d14efd0dbec55f7c42fd9a34963936ab
Cristiana Sorrenti <cristianasorrenti@gmail.com>
2026-07-09 12:46:15 +0200

    aggiunta registrazione libri

--- src/main/java/it/corso/Main.java
+++ src/main/java/it/corso/Main.java
@@ -2,6 +2,13 @@ package it.corso;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+
+        LibraryService libreria = new LibraryService();
+        libreria.addBook( new Book(1, "titolo1", "Nicolò") );
+        libreria.addBook( new Book(2, "titolo2", "Giacomo") );
+        libreria.addBook( new Book(3, "titolo3", "Edoardo") );
+        libreria.addBook( new Book(4, "titolo4", "Giovanna") );
+
     }
 }
