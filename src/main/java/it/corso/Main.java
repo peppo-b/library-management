176f4987bc7a9682c1e4f9a017ab64bd02bf5282
Giuseppe Bruno <giuseppe.bruno@finconsgroup.com>
2026-07-09 16:44:14 +0200

    bug fix su casting di tipo

--- src/main/java/it/corso/Main.java
+++ src/main/java/it/corso/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         LibraryService libreria = new LibraryService();
-        libreria.addBook( new Book(1, "titolo1", "Nicolò") );
-        libreria.addBook( new Book(2, "titolo2", "Giacomo") );
-        libreria.addBook( new Book(3, "titolo3", "Edoardo") );
-        libreria.addBook( new Book(4, "titolo4", "Giovanna") );
+        libreria.addBook( new Book((long)1, "titolo1", "Nicolò") );
+        libreria.addBook( new Book((long)2, "titolo2", "Giacomo") );
+        libreria.addBook( new Book((long)3, "titolo3", "Edoardo") );
+        libreria.addBook( new Book((long)4, "titolo4", "Giovanna") );
 
         libreria.listBooks();
 
